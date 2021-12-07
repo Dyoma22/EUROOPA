739c9a2432de50789b4c49c06b108298ef31eb05
opilane <opilane@HP-CZC6297B9B>
2021-12-07 13:44:30 +0200

    Add project files.

---
+++ EUROOPA.py
@@ -0,0 +1,12 @@
+#Задание 1
+Capitals={} 
+Capitals = dict()
+Capitals["Europaa"]=[""]
+Countries[""]
+Country=[""]
+
+for country in Countries:
+	if country in Capitals:
+		print("Столица страны"+ country + ":"+Capitals[country])
+	else:
+		print('В базе нет страны c названием ' + country)
