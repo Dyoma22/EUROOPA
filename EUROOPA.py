3641bfc317cce1def75c90a1bb4e0ccd9763945c
opilane <opilane@HP-CZC6297B9B>
2021-12-08 12:22:17 +0200

    xd

--- EUROOPA.py
+++ EUROOPA.py
@@ -1,17 +1,48 @@
-from module1 import*
-Capitals={} 
+Capitals={}
 Capitals=dict()
-Countries={}
-Countries=dict()
-Capitals=listt("stranq.txt")
-Countries=listt("stolica.txt")
+Capitals=["Austria","Azerbaijan","Albania","Andorra","Armenia","Belarus","Belgium","Bulgaria","Bosnia and Herzegovina","Vatican","United Kingdom","Hungary","Germany","Greece","Georgia","Denmark","Ireland","Iceland","Spain","Italy","Kazakhstan","Cyprus","Latvia","Lithuania","Liechtenstein","Luxembourg","Malta","Moldova","Monaco","Netherlands","Norway","Poland","Portugal","RepublicofMacedonia","Russia","Romania","SanMarino","Serbia","Slovakia","Slovenia","Turkey","Ukraine","Finland","France","Croatia","Montenegro","Czech","Switzerland","Sweden","Estonia"]
+Countries=["Vein","Baku","Tirana","Andorra la Vella","Yerevan","Minsk","Brussels","Sofia","Sarajevo","Vatican","London","Budapest","Berlin","Athens","Tbilisi","Copenhagen","Dublin","Reykjavik","Madrid","Rome","Astana","Nicosia","Riga","Vilnius","Vaduz","Luxembourg","Valletta","Kishinev","Monaco","Amsterdam","Oslo","Warsaw","Lisbon","Skopje","Moscow","Bucharest","San Marino","Belgrade","Bratis", "lava","Ljubljana","Ankara","Kiev","Helsinki","Paris","Zagreb","Podgorica","Prague","Berne","Stockholm","Tallinn"]
+
+while True:
+	print("Добро пожаловать!")
+	a=input("Напишите страну или столицу - ")
+	if a=="Austria":
+		print("Vein")
+	elif a=="Azerbaijan":
+		print("Baku")
+	elif a=="Albania":
+		print("Tirana")
+	elif a=="Andorra":
+		print("Andorra la Vella")
+	elif a=="Armenia":
+		print("Yerevan")
+	elif a=="Belarus":
+		print("Minsk")
+	elif a=="Belgium":
+		print("Brussels")
+	elif a=="Bulgaria":
+		print("Sofia")
+	elif a=="Bosnia and Herzegovina":
+		print("Sarajevo")
+	elif a=="Albania":
+		print("Tirana")
+	elif a=="Albania":
+		print("Tirana")
+
+
+
+
+
+
+
+
+
 
 
 for country in Countries:
 	if country in Capitals:
-		print("Столица страны"+ country + ":"+Capitals[country])
+		print("Столица страны " + country + ": " + Capitals[country])
 	else:
-		print('В базе нет страны c названием ' + country)
+		print("В базе нет страны c названием " + country)
 
-stranq=listt("stranq.txt")
-stolica=listt("stolica.txt")
+
