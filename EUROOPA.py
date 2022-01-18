c1770aeb72e32127a739d04d99523417dcccb613
opilane <opilane@HP-CZC6297B8P>
2022-01-18 11:29:55 +0200

    xd

--- EUROOPA.py
+++ EUROOPA.py
@@ -1,68 +1,72 @@
 from module1 import*
 from random import*
-from gtts import gTTS
-Capitals={}
+from time import*
 Capitals=dict()
-Capitals["Austria"]="Vienna"
+Capitals["Estonia"]="Tallinn"
 Capitals["Albania"]="Tirana"
 Capitals["Belgium"]="Brussels"
 Capitals["Czechia"]="Prague"
 Capitals["Poland"]="Warsaw"
 Capitals["Portugal"]="Lisbon"
-Capitals["Germany"]="Berlin"
-Capitals["Sweden"]="Stockholm"
-Capitals["Spain"]="Madrid"
-Capitals["Serbia"]="Belgrade"
-Capitals["Norway"]="Oslo"
-Capitals["Moldova"]="Chisinau"
-Capitals["Greece"]="Athens"
-Capitals["Bulgaria"]="Sofia"
-Capitals["France"]="Paris"
 Capitals["Finland"]="Helsinki"
-Capitals["Estonia"]="Tallinn"
-Countries=["Austria","Albania","Belgium","Czechia","Poland","Portugal","Germany","Sweden","Spain","Serbia","Norway","Moldova","Greece","Bulgaria","France","Finland","Estonia"]
-while True:
-	w=input("Функция:\nНайти страну - 1\nДобавить страну - 2")
-	if w=="1":
-		count()
-	elif w=="2":
-		country()
-
-
-
-
-		
-		
-
-	
-		
-
-
-		
-
-		 
-		 
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-s=gTTS(text=Countries[0],lang="et",slow=True).save("heli.mp3")
-os.system("heli.mp3")
-
-
+Capitals["France"]="Paris"
+Capitals["Germany"]="Berlin"
+Capitals=["Tallinn","Tirana","Brussels","Prague","Warsaw","Lisbon","Helsinki","Paris","Berlin"]
+Countries=["Estonia","Albania","Belgium","Czechia","Poland","Portugal","Finland","France","Germany"]
+for country in Countries:
+    country=input("Введите страну: ")
+    if country in Capitals:
+        print("Столица страны "+country+": " +Capitals[country])
+        p=input("Возможно в базе данных ошибка, хотите исправить её? ")
+        if p=="Да":
+            o=input("Введите правильно страну: ")
+            l=input("Введите правильно столицу: ")
+            Capitals.pop(country)
+            Capitals.update({o: l})
+        elif p=="Нет":
+            print("Всего доброго!")
+    else:
+        print("В базе данных не страны с названием " +country)
+        v=input("Хотите внести " +country+ " в базу данных?: ")
+        if v=="Да":
+            ca=input("Введите столицу страны "+country)
+            Capitals.update({country: ca})
+        elif v=="Нет":
+            print("Всего доброго!")
+    p=input("Хотите ли пройти тест на знания столиц Европы? Да или Нет? ")
+    if p=="Да":
+        Countries.sort()
+        Countries.reverse()
+        m=0
+        for i in range(10):
+            print(choices(Countries))
+            st=input("Введите столицу: ")
+            if st in Capitals:
+                    print("Правильно!")
+                    m+=1
+            else:
+                print("Неправильно!")
+        if m==0:
+            print("0%")
+        elif m==1:
+            print("10%")
+        elif m==2:
+            print("20%")
+        elif m==3:
+            print("30%")
+        elif m==4:
+            print("40%")
+        elif m==5:
+            print("50%")
+        elif m==6:
+            print("60%")
+        elif m==7:
+            print("70%")
+        elif m==8:
+            print("80%")
+        elif m==9:
+            print("90%")
+        elif m==10:
+            print("100%")
+    if p=="Нет":
+        print("Всего доброго!")
